77d98e3c878b98a6d9ba6533c83de36b6702d97b
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-06-28 19:15:38 +0200

    commit

--- SqlReporter.py
+++ SqlReporter.py
@@ -1,11 +1,14 @@
 import sys
 import os
 import os.path
+import cx_Oracle
+import csv
 from sys import exit
 from os import listdir
 
-from properties import REPORTS_PATH, SQL_PATH
+from properties import REPORTS_PATH, SQL_PATH, headers, delimiter
 from LoggerSetup import LoggerSetup
+from oracle_config import user, passwd, host, port, sid
 
 log = LoggerSetup().get_logger('SqlReporter')
 
@@ -41,15 +44,46 @@ class SqlReporter():
                 log.error("Invalid value for request. The script ends.", exc_info=True)
                 sys.exit(1)
         if os.stat(sql_file).st_size != 0:
-            self.execute_sql(sql_file)
+            self.execute_sql(sql_file, delimiter, REPORTS_PATH)
+            
         else:
             log.warning('Chosen file {} is empty. The script ends.'.format(sql_file))
             sys.exit(1)
 
 
-    def execute_sql(self, file):
-        pass
+    def execute_sql(self, file, deli, path):
+        log.debug('Calling oracle_insert() method.')
+        try:
+            dsn = cx_Oracle.makedsn(host, port, sid)
+            conn = cx_Oracle.connect(
+                user = user,
+                password = passwd, 
+                dsn = dsn
+            )
+            cur = conn.cursor()
+            with open(file, 'r') as fp:
+                sql = fp.read()
+            cur.execute(sql)
+            row = cur.fetchall()
+            report_file = path + file.split('/')[-1].split('.')[0] + '.csv'
+            with open(report_file, 'a', newline='') as report:
+                a = csv.writer(report, delimiter=deli)
+                a.writerows(row)
+            log.info('Report generation for {} database completed.'.format(dsn))
+        except Exception as ex:
+            log.error('An error occured while connecting to database: {}'.format(ex), exc_info=True)
+            sys.exit(1)
+        finally:
+            try:
+                cur.close()
+                conn.close()
+            except NameError as nerr:
+                pass
 
+
+    def add_headers(self, header_check, headers):
+        if header_check:
+            pass
 
     def check_directory(self, path):
         log.debug('Calling check_directory() method for {}.'.format(path))
