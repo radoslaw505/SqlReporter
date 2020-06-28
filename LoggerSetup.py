1ef7341e365c75d040fb438c76e862ad0d7e22df
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-06-28 23:00:32 +0200

    commit

--- LoggerSetup.py
+++ LoggerSetup.py
@@ -3,13 +3,12 @@ import time
 import os
 import os.path
 
-from properties import LOG_PATH
 
 class LoggerSetup():
-    def get_logger(self, ctrl_file):
-        if not os.path.exists(LOG_PATH):
-            os.mkdir(LOG_PATH)
-        control_file = LOG_PATH + ctrl_file + time.strftime("%Y%m%d-%H%M%S") + '.log'
+    def get_logger(self, ctrl_file, path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+        control_file = path + ctrl_file + time.strftime("%Y%m%d-%H%M%S") + '.log'
         log = logging.getLogger('log')
         logging.basicConfig(
             format='[%(asctime)s][%(process)-6s][%(levelname)-7s]: %(message)s',
